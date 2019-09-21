d02a6a5b5cd8e240ea1966947d91dafe2e247639
qc366 <22866665@qq.com>
2019-09-21 17:55:48 +0800

    基础框架，演示hello

--- springboot2_hello/src/main/java/com/qc366/AppHello.java
+++ springboot2_hello/src/main/java/com/qc366/AppHello.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class AppHello {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(AppHello.class, args);
     }
 }
