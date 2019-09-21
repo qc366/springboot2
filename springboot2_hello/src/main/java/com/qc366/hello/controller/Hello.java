6ac9f98b4f4aca7aa311d373ec7adb1e618c9c5a
qc366 <22866665@qq.com>
2019-09-21 16:48:30 +0800

    基础框架，演示hello

---
+++ springboot2_hello/src/main/java/com/qc366/hello/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.qc366.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
+
+}
